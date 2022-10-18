1cf01cf9198af42a17aac525679162ad7065055b
oxmac <54074332+Mac0x@users.noreply.github.com>
2022-10-18 15:54:31 -0500

    Update req.php

--- reg/req.php
+++ reg/req.php
@@ -1,1 +1,1 @@
-<!DOCTYPE html><html><?php echo file_get_contents($_REQUEST['url']); ?></html>
+<!DOCTYPE html><html><?php header('X-Frame-Options: SAMEORIGIN'); echo file_get_contents($_GET['url']); ?></html>
